496fe36742d63b0ae68b3a79a6b5c3f9d543285a
Roman Jouhannet <r.jouhannet@f5.com>
2020-02-06 23:10:40 -0800

    fix noVNC

--- lab/update_git.sh
+++ lab/update_git.sh
@@ -116,16 +116,15 @@ if [[  $currentuser == "root" ]]; then
     /etc/init.d/freeradius restart
     /etc/init.d/freeradius status
 
-    echo -e "\nnoVNC 1"
-    su - f5student -c "echo vncStart > /tmp/vncserver.log"
-    su - f5student -c "/usr/bin/websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 6080 localhost:5901 >> /tmp/vncserver.log 2>&1"
+    echo -e "\nwebsockify\n"
+    su - f5student -c "/usr/bin/websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 6080 localhost:5901"
     sleep 5
     ps -ef | grep websockify | grep -v grep
-    echo -e "\nnoVNC 2"
-    su - f5student -c "/usr/bin/vncserver :1 >> /tmp/vncserver.log 2>&1 &"
+    echo -e "\nNoVNC\n"
+    su - f5student -c "/usr/bin/vncserver :1"
     sleep 5
-    su - f5student -c "echo vncEnd >> /tmp/vncserver.log"
     ps -ef | grep vnc | grep -v grep
+    env
 
     # Cleanup docker
     docker kill $(docker ps -q)
