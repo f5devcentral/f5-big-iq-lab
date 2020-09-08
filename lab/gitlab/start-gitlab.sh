023d5c066f7aa64716ea32cb2914973bdf0c30a1
Roman Jouhannet <r.jouhannet@f5.com>
2020-09-08 11:50:44 -0700

    update gitlab permission again

--- lab/gitlab/start-gitlab.sh
+++ lab/gitlab/start-gitlab.sh
@@ -7,7 +7,7 @@ ssh-keygen -f "/home/f5student/.ssh/known_hosts" -R "[localhost]:7022"
 
 GITLAB_HOME="~/gitlab/" docker-compose -f ~/gitlab/docker-compose.yml up -d
 
-secs=120
+secs=300
 while [ $secs -gt 0 ]; do
         echo -ne "$secs\033[0K\r"
         sleep 1
@@ -20,7 +20,7 @@ docker exec gitlab_gitlab_1 gitlab-ctl status
 docker exec -it gitlab_gitlab_1 update-permissions
 docker exec gitlab_gitlab_1 chown -R git:git /var/opt/gitlab/gitaly
 docker exec gitlab_gitlab_1 chown -R gitlab-prometheus:gitlab-prometheus /var/opt/gitlab/grafana/data
-sleep 2
+sleep 1
 docker restart gitlab_gitlab_1
 
 secs=120
@@ -29,8 +29,6 @@ while [ $secs -gt 0 ]; do
         sleep 1
         : $((secs--))
 done
-
-docker logs gitlab_gitlab_1
 
 docker exec gitlab_gitlab_1 gitlab-ctl status
 
