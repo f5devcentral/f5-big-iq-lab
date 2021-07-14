1b9604f91325fa523644cfc2533da3862afd2b13
Roman Jouhannet <r.jouhannet@f5.com>
2021-07-14 10:27:44 -0700

    setup workflow

---
+++ containthedocs-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -x
+
+COMMAND=(/bin/bash -c "sudo pip install -r requirements.txt -U ; make -C docs clean html")
+
+. ./containthedocs-image
+
+exec docker run --rm -t \
+  -v "$PWD":"$PWD" --workdir "$PWD" \
+  ${DOCKER_RUN_ARGS} \
+  -e "LOCAL_USER_ID=$(id -u)" \
+  ${DOC_IMG} "${COMMAND[@]}"
